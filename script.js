909bc3e16d84e608376d8e091dbc5651916dac2c
@SarxRom <2233073251@correo.ler.uam.mx>
2025-04-15 09:07:26 -0600

    cambio

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-console.log("El colectivo Punto Rojo programo esta página")
-window.alert("El contenido que se muestra a continuación no es apto para menores de 15 años");
+function redirect(url) {
+    window.location.href = url;
+}
