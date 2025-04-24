a04ba965f89891f568be3452a4ade334eaa1942c
@SarxRom <2233073251@correo.ler.uam.mx>
2025-04-23 21:39:31 -0600

    paz

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("El colectivo Punto Rojo programo esta página")
+window.alert("Para una mejor experiencia favor de usar audifonos");
 function redirect(url) {
     window.location.href = url;
 }
